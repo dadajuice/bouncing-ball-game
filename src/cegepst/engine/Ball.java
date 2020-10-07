20c7b019aaa2583ed097875ac75128423930ce45
dadajuice <davidt2003@msn.com>
2020-10-07 16:59:30 -0400

    Refactoring ajout de la classe Ball

---
+++ src/cegepst/engine/Ball.java
@@ -0,0 +1,60 @@
+package cegepst.engine;
+
+import java.util.Random;
+
+public class Ball {
+
+    private final int radius;
+    private int x;
+    private int y;
+    private int velocityX;
+    private int velocityY;
+
+    public Ball(int radius) {
+        this.radius = radius;
+        x = getRandom(radius * 2, 800 - radius * 2);
+        y = getRandom(radius * 2, 600 - radius * 2);
+        velocityX = getRandom(0, 1) == 0 ? 2 : -2;
+        velocityY = getRandom(0, 1) == 0 ? 2 : -2;
+    }
+
+    public void update() {
+        x += velocityX;
+        y += velocityY;
+        if (hasTouchVerticalBound()) {
+            velocityY *= -1;
+        }
+        if (hasTouchHorizontalBound()) {
+            velocityX *= -1;
+        }
+    }
+
+    public boolean hasTouchBound() {
+        return hasTouchHorizontalBound() || hasTouchVerticalBound();
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    private boolean hasTouchHorizontalBound() {
+        return x <= radius || x >= 800 - radius;
+    }
+
+    private boolean hasTouchVerticalBound() {
+        return y <= radius || y >= 600 - radius;
+    }
+
+    private int getRandom(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
+    }
+}
--- src/cegepst/engine/GameWindow.java
+++ src/cegepst/engine/GameWindow.java
@@ -10,11 +10,7 @@ public class GameWindow extends JFrame {
     private static final int SLEEP = 25;
     private long before;
     private JPanel panel;
-    private int radius = 25;
-    private int x;
-    private int y;
-    private int dx;
-    private int dy;
+    private Ball ball;
     private int score = 0;
     private boolean playing = true;
     private BufferedImage bufferedImage;
@@ -34,10 +30,7 @@ public class GameWindow extends JFrame {
         panel.setDoubleBuffered(true);
         super.add(panel);
 
-        x = getRandom(0 + radius * 2, 800 - radius * 2);
-        y = getRandom(0 + radius * 2, 600 - radius * 2);
-        dx = getRandom(0, 1) == 0 ? 2 : -2;
-        dy = getRandom(0, 1) == 0 ? 2 : -2;
+        ball = new Ball(25);
     }
 
     public void start() {
@@ -75,21 +68,16 @@ public class GameWindow extends JFrame {
     }
 
     private void update() {
-        x += dx;
-        y += dy;
-        if (y <= radius || y >= 600 - radius) {
-            dy *= -1;
-            score += 10;
-        }
-        if (x <= radius || x >= 800 - radius) {
-            dx *= -1;
+        ball.update();
+        if (ball.hasTouchBound()) {
             score += 10;
         }
     }
 
     private void drawOnBuffer() {
         buffer.setPaint(Color.red);
-        buffer.fillOval(x, y, radius * 2, radius * 2);
+        buffer.fillOval(ball.getX(), ball.getY(), ball.getRadius() * 2,
+                ball.getRadius() * 2);
 
         buffer.setPaint(Color.white);
         buffer.drawString("Score: " + score, 10, 20);
@@ -100,10 +88,5 @@ public class GameWindow extends JFrame {
         graphics2D.drawImage(bufferedImage, 0, 0, panel);
         Toolkit.getDefaultToolkit().sync();
         graphics2D.dispose();
-    }
-
-    private int getRandom(int min, int max) {
-        Random random = new Random();
-        return random.nextInt((max - min) + 1) + min;
     }
 }
